5fc2dc7384d7b77ef280979c8c481e690c2d8d8c
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2024-07-21 18:20:38 +0400

    create three object instances

--- BethanysPieShopHRM/Employee.cs
+++ BethanysPieShopHRM/Employee.cs
@@ -5,9 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace BethanysPieShopHRM
-{
-    internal class Employee
     {
+    internal class Employee
+        {
         public string firstName;
         public string lastName;
         public string email;
@@ -20,32 +20,55 @@ namespace BethanysPieShopHRM
 
         const int minimalHoursWorkedUnit = 1;
 
-         
+        // adding custom constructor
+        // overload constructor, in this case hourlyrate is 0 
+        public Employee( string firstN, string lastN, string em, DateTime bd ) : this(firstN, lastN, em, bd, 0)
+            {
+
+            }
+
+        public Employee( string firstN, string lastN, string em, DateTime bd, double rate )
+            {
+            firstName = firstN;
+            lastName = lastN;
+            email = em;
+            birthday = bd;
+            hourlyRate = rate;
+            }
+
+        //public Employee()
+        //    {
+
+        //    }
+
+        // performed work from employees
         public void PerformWork()
-        {
+            {
             //numberOfHoursWorked++;
             PerformWork(minimalHoursWorkedUnit);
             //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
-        }
-        public void PerformWork(int numberOfHours)
-        {
+            }
+        public void PerformWork( int numberOfHours )
+            {
             numberOfHoursWorked += numberOfHours;
             Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
-        }
-        public double ReceiveWage(bool resetHours = true)
-        {
+            }
+        // method to calculate employee wage
+        public double ReceiveWage( bool resetHours = true )
+            {
             wage = numberOfHoursWorked * hourlyRate;
 
             Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
-            if(resetHours)
+            if (resetHours)
                 numberOfHoursWorked = 0;
 
             return wage;
-        }
+            }
+        // displaying employee details
         public void DisplayEmployeeDetails()
-        {
+            {
             Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthday.ToShortDateString()}\n");
-        }
+            }
 
+        }
     }
-}
--- BethanysPieShopHRM/Program.cs
+++ BethanysPieShopHRM/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+
+using BethanysPieShopHRM;
+
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee bethany = new Employee("Bethany", "Smith", "betany12@outlook.com", new DateTime(1979, 1, 16), 25);
+
+bethany.DisplayEmployeeDetails();
+
+bethany.PerformWork(3);
+bethany.PerformWork(6);
+bethany.PerformWork(5);
+bethany.PerformWork(7);
+
+double receievedWageBethany = bethany.ReceiveWage(true);
+
+Console.WriteLine($"Wage paid (message from program): {receievedWageBethany}");
+
+Console.WriteLine("\n");
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee maria = new ("Maria", "Jones", "mariaJones@hotmail.com", new DateTime(1970, 12, 12), 30);
+
+maria.DisplayEmployeeDetails();
+
+maria.PerformWork(4);
+maria.PerformWork(3);
+maria.PerformWork(4);
+maria.PerformWork(10);
+
+var receivedWageMaria = maria.ReceiveWage(true);
+
+Console.WriteLine($"Wage paid (message from program) {receivedWageMaria}");
+
+
+Console.WriteLine("\n");
+Console.WriteLine("Creating an employee");
+Console.WriteLine("--------------------\n");
+
+Employee george = new Employee("George", "Washington", "gwashington@mail.ru", new DateTime(1975, 9, 30), 28);
+
+george.DisplayEmployeeDetails();
+
+george.PerformWork(10);
+george.PerformWork(5);
+george.PerformWork(2);
+george.PerformWork();
+
+var receivedWageGeorge = george.ReceiveWage(true);
+
+Console.WriteLine($"Wage paid (message from program): {receivedWageGeorge}");
+//
+
+
