439e107aad3d44e3c0b1004fe0cef589bb2281f9
Gmentesh2 <g.menteshashvili.sg18@gipa.ge>
2024-07-21 16:36:37 +0400

    Add project files.

---
+++ BethanysPieShopHRM/Employee.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BethanysPieShopHRM
+{
+    internal class Employee
+    {
+        public string firstName;
+        public string lastName;
+        public string email;
+
+        public int numberOfHoursWorked;
+        public double wage;
+        public double hourlyRate;
+
+        public DateTime birthday;
+
+        const int minimalHoursWorkedUnit = 1;
+
+         
+        public void PerformWork()
+        {
+            //numberOfHoursWorked++;
+            PerformWork(minimalHoursWorkedUnit);
+            //Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHoursWorked} hour(s)!");
+        }
+        public void PerformWork(int numberOfHours)
+        {
+            numberOfHoursWorked += numberOfHours;
+            Console.WriteLine($"{firstName} {lastName} has worked for {numberOfHours} hour(s)!");
+        }
+        public double ReceiveWage(bool resetHours = true)
+        {
+            wage = numberOfHoursWorked * hourlyRate;
+
+            Console.WriteLine($"{firstName} {lastName} has received a wage of {wage} for {numberOfHoursWorked} hour(s) of work.");
+            if(resetHours)
+                numberOfHoursWorked = 0;
+
+            return wage;
+        }
+        public void DisplayEmployeeDetails()
+        {
+            Console.WriteLine($"\nFirst name: \t{firstName}\nLast name: \t{lastName}\nEmail: \t\t{email}\nBirthday: \t{birthday.ToShortDateString()}\n");
+        }
+
+    }
+}
